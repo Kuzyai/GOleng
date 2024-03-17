d42c55d6a00d0d4b5a8edbcfa70c07a4ab45fcdc
Konstantin <ognev.k.a@mail.ru>
2024-03-17 11:19:39 +0300

    change first programm

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
-import "fmt"
-
 func main() {
-	var grad, hour, minutes, indexHour, indexMinutes, i uint16
-	indexHour = 360 / 12
-	indexMinutes = 12 * 60 / 360
-	fmt.Scan(&grad)
-	if grad <= 360 {
-		hour = grad / indexHour
-		i = grad - hour*indexHour
-		minutes = i * indexMinutes
-		fmt.Println("It is", hour, "hours", minutes, "minutes.")
-	}
+	//var grad, hour, minutes, indexHour, indexMinutes, i uint16
+	//indexHour = 360 / 12
+	//indexMinutes = 12 * 60 / 360
+	//fmt.Print("Ввседите количество градусов часовой стрелки от 0 до 360, чтобы узнать сколько времени: ")
+	//fmt.Scan(&grad)
+	//if grad <= 360 {
+	//	hour = grad / indexHour
+	//	i = grad - hour*indexHour
+	//	minutes = i * indexMinutes
+	//	fmt.Println("It is", hour, "hours", minutes, "minutes.")
+	//}
 }
