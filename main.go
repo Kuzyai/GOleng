4de210ca7dfdcd7dc6273be9100cd00acb315d12
Konstantin <ognev.k.a@mail.ru>
2024-03-24 02:33:26 +0300

    calculator version 1

--- main.go
+++ main.go
@@ -1,27 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	cityPopulation := map[string]int{
-		"Ryazan":     528,
-		"Novouralsk": 78,
-		"Tomsk":      556,
-		"Kemerovo":   557,
-	} //город: население города в тысячах человек,
-	groupCity := map[int][]string{
-		10:   {"Kuznetsk", "Bor", "Novouralsk"},             // города с населением 10-99 тыс. человек
-		100:  {"Ryazan", "Tomsk", "Kemerovo"},               // города с населением 100-999 тыс. человек
-		1000: {"Moscow", "Saint Petersburg", "Novosibirsk"}, // города с населением 1000 тыс. человек и более
+	var result float64
+	value1, value2, operation := readTask()
+	a, err := doFloat(value1)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	for key, _ := range cityPopulation {
-		for key1, value1 := range groupCity {
-			for _, i := range value1 {
-				if key == i && key1 != 100 {
-					delete(cityPopulation, key)
-				}
-			}
+	b, err := doFloat(value2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	operation, err = doSign(operation)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	switch operation {
+	case "+":
+		result = func(v1, v2 float64) float64 { return v1 + v2 }(a, b)
+	case "-":
+		result = func(v1, v2 float64) float64 { return v1 - v2 }(a, b)
+	case "*":
+		result = func(v1, v2 float64) float64 { return v1 * v2 }(a, b)
+	case "/":
+		result = func(v1, v2 float64) float64 { return v1 / v2 }(a, b)
+	}
+	fmt.Printf("%.4f", result)
+}
+func readTask() (interface{}, interface{}, interface{}) {
+	var value1, value2, operation interface{}
+	r := bufio.NewReader(os.Stdin)
+	value1, _ = r.ReadString('\n')
+	value2, _ = r.ReadString('\n')
+	operation, _ = r.ReadString('\n')
+	return value1, value2, operation
+}
+func doSign(i interface{}) (string, error) {
+	if v, ok := i.(string); ok {
+		v = strings.Trim(v, "\n")
+		if v == "+" || v == "-" || v == "*" || v == "/" {
+			return v, nil
 		}
+		return "", fmt.Errorf("неизвестная операция")
 	}
-	fmt.Println(cityPopulation)
+	return "", fmt.Errorf("неизвестная операция")
+}
+func doFloat(i interface{}) (float64, error) {
+	var s string
+	if v, ok := i.(string); ok {
+		s = v
+	}
+	s = strings.Trim(s, "\n")
+	n, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0, fmt.Errorf("value = %v: %T", n, n)
+	}
+	return n, nil
 }
