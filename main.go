a1355329bbd8186f2212b0a9d2d62887056d6990
Konstantin <ognev.k.a@mail.ru>
2024-03-26 22:57:15 +0300

    calculation of the monthly loan payment

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
-import (
-	"bufio"
-	"os"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	var sum int
-	s := bufio.NewScanner(os.Stdin)
-	w := bufio.NewWriter(os.Stdout)
-	for s.Scan() {
-		num, err := strconv.Atoi(s.Text())
-		if err != nil {
-			break
-		}
-		sum += num
+	var persentMonth1, persentMonth2, sum1, sum2 float64
+	var cost1 float64 = 720000
+	var cost2 float64 = 1047400
+	var persent1 float64 = 0.3499
+	var persent2 float64 = 0.1499
+	for i := 1; i <= 60; i++ {
+		persentMonth1 = (cost1 * persent1) / 12
+		persentMonth2 = (cost2 * persent2) / 12
+		cost1 += persentMonth1
+		cost2 += persentMonth2
+		cost1 -= 25548
+		cost2 -= 24912
+		sum1 += persentMonth1
+		sum2 += persentMonth2
+		fmt.Println(i, fmt.Sprintf("%.2f", cost1), fmt.Sprintf("%.2f", cost2))
 	}
-	_, err := w.WriteString(strconv.Itoa(sum))
-	if err != nil {
-		panic(err)
-	}
-	w.Flush()
+
+	fmt.Println(" ")
+	fmt.Println(fmt.Sprintf("%.2f", sum1+720000), fmt.Sprintf("%.2f", sum2+1047400))
 }
